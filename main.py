3ab2da6eda94929ceec7ed9a6306f0b94542967d
sunfanyi <sunfanyi_jim@outlook.com>
2023-03-18 17:31:48 +0000

    Spatial Transformation on car.

---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D, art3d
+
+from tools_cv import *
+# from tools_kinematics import *
+import sympy as sp
+
+
